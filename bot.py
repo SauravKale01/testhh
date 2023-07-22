38d31245f6dce7c96ab06100a30db7c000243978
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-07-22 11:05:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,14 @@
 import httpx
 from pyrogram import filters, Client, idle
 from pyrogram.types import Message
-from pymongo import MongoClient
 
 # Replace these with your actual values
 API_ID = 19099900
 API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
 BOT_TOKEN = "6390766852:AAHAXsP3NHPX2NbnRaFDZA9ZH1h6FyNH1K4"
-MONGODB_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"
-ADMIN_USER_ID = '6198858059'
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# MongoDB client and database setup
-mongo_client = MongoClient(MONGODB_URI)
-db = mongo_client["my_bot_db"]
-user_profiles = db["user_profiles"]
 
 # Dictionary to store the conversation history for each user
 conversation_history = {}
@@ -91,67 +84,6 @@ async def clear_history(_: Client, message: Message):
         await message.reply("Chat history cleared.")
     else:
         await message.reply("Chat history is already empty.")
-
-@app.on_message(filters.command("broadcast") & filters.user(ADMIN_USER_ID))
-async def broadcast(_: Client, message: Message):
-    if len(message.command) < 2:
-        return await message.reply("Please provide a message to broadcast.")
-
-    msg_to_broadcast = message.text.split(maxsplit=1)[1]
-
-    # Get all user IDs to broadcast the message
-    user_ids = [str(user["_id"]) for user in user_profiles.find({}, {"_id": 1})]
-
-    for user_id in user_ids:
-        try:
-            await app.send_message(int(user_id), msg_to_broadcast)
-        except Exception as e:
-            print(f"Failed to send message to user {user_id}: {str(e)}")
-
-@app.on_message(filters.private)
-async def process_dm(client: Client, message: Message):
-    # Handle DM messages without a specific command
-    txt = await message.reply("Typing....")
-
-    query = message.text
-
-    # Retrieve conversation history for this user
-    chat_id = message.from_user.id
-    if chat_id in conversation_history:
-        dialog_messages = conversation_history[chat_id]
-    else:
-        dialog_messages = []
-
-    url = "https://api.safone.me/chatgpt"
-    payload = {
-        "message": query,
-        "chat_mode": "assistant",
-        "dialog_messages": dialog_messages,
-    }
-
-    async with httpx.AsyncClient(timeout=20) as client:
-        try:
-            response = await client.post(
-                url, json=payload, headers={"Content-Type": "application/json"}
-            )
-            response.raise_for_status()
-            results = response.json()
-
-            # Check if the API response contains the 'message' key
-            if "message" in results:
-                bot_response = results["message"]
-
-                # Update conversation history with the latest message
-                dialog_messages.append({"bot": bot_response, "user": query})
-                conversation_history[chat_id] = dialog_messages
-
-                await txt.edit(bot_response)
-            else:
-                await txt.edit("**An error occurred. No response received from the API.**")
-        except httpx.HTTPError as e:
-            await txt.edit(f"**An HTTP error occurred: {str(e)}**")
-        except Exception as e:
-            await txt.edit(f"**An error occurred: {str(e)}**")
 
 # Run the bot
 app.run()
