74daf53ac0b8e75736b5e34e410373e394ef8c89
SauravKale01 <139038240+SauravKale01@users.noreply.github.com>
2023-08-02 23:38:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,10 +104,6 @@ def ask_bard(question):
 async def ask_command_handler(client: Client, message: Message):
     question = " ".join(message.command[1:])
     if question:
-        # Show typing status to simulate processing
-        await message.reply_chat_action("typing")
-        time.sleep(2)  # Simulate processing time
-
         response_content = ask_bard(question)
         await message.reply(response_content)
     else:
